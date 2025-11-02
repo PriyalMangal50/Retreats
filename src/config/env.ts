13d7351d695973b87d679d2ac2db840baa3b1b6e
adityakkpk <akkpk933@gmail.com>
2025-11-02 16:19:02 +0530

    API url added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL?.replace(/\/+$/, "") ||
-  "http://localhost:8000/api";
+  "https://retreatsbytraveon-api.onrender.com/api";
+  // "http://localhost:8000/api";
